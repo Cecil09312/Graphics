8dbfd8af8ceb0b8e18e4e960db82d8ffdc717c24
Song <1215223488@qq.com>
2018-11-02 17:23:34 +0800

    添加glWidget.h

---
+++ openglWidget/glwidget.h
@@ -0,0 +1,11 @@
+#ifndef GDelegateModelH
+#define GLWIDGET_H
+
+#include <QOpenGLWidget>
+class GLWidget : public QOpenGLWidget
+{
+public:
+    GLWidget(QWidget *parent = nullptr);
+};
+
+#endif // GLWIDGET_H
